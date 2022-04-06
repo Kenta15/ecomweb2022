59523bee8f09ad7d67d9425aa75b9e960534968b
Kenta Tanaka <tkenta2929@gmail.com>
2022-04-05 17:59:55 -0700

    copied images

---
+++ staticfiles/js/cart.js
@@ -0,0 +1,67 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for(var i=0;i<updateBtns.length;i++){
+    updateBtns[i].addEventListener('click',function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:',productId,'action:',action)
+        console.log('USER:', user)
+        if(user=='AnonymousUser'){
+            addCookieItem(productId,action)
+        }
+        else{
+            updateUserOrder(productId,action)
+        }
+    })
+}
+
+function addCookieItem(productId,action){
+
+    if(action == 'add'){
+        if(cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+        }
+        else{
+            cart[productId]['quantity'] += 1
+        }
+        location.reload()
+    }
+    if(action == 'remove'){
+        cart[productId]['quantity'] -= 1
+        
+        if(cart[productId]['quantity'] <= 0){
+            console.log('Remove Item')
+            delete cart[productId]
+        }
+        location.reload()
+    }
+
+    console.log('cart:',cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/" 
+}
+
+function updateUserOrder(productId,action){
+
+    var url = '/update_item/'
+
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({'productId':productId,'action':action})
+    })
+
+    // let .then = response => response.json()
+    // let .then = data => console.log('data:',data)
+
+    .then((response)=>{
+        response.json()
+    })
+
+    .then((data)=>{
+        console.log('data:',data)
+        location.reload()
+    })
+}
---
+++ staticfiles/js/country.js
@@ -0,0 +1,50 @@
+var element = document.getElementById('country-btn')
+
+element.addEventListener("click", function(){
+    var country = document.getElementById("country")
+    var selected_country = country.options[country.selectedIndex].value;
+    if(user=='AnonymousUser'){
+        addCookieCountry(selected_country)
+        alert('Please login to change the country')
+    }
+    else{
+        updateCountry(selected_country)
+        alert('Country was changed')
+    }
+});
+
+function addCookieCountry(selected_country){
+
+    country = selected_country
+
+    location.reload()
+
+    console.log('country:',country)
+
+    document.cookie = 'country=' + JSON.stringify(country) + ";domain=;path=/"
+}
+
+function updateCountry(selected_country){
+
+    var url = '/update_country/'
+
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({'selected_country':selected_country})
+    })
+    // let .then = response => response.json()
+    // let .then = data => console.log('data:',data)
+
+    .then((response)=>{
+        response.json()
+    })
+
+    .then((data)=>{
+        console.log('data:',data)
+        location.reload()
+    })
+}
